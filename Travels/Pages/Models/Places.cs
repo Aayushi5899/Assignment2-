2642e42e81821e4349237866b23457cf3e69f1dd
Aayushi5899 <W0787383@myscc.ca>
2021-09-29 17:19:29 -0400

    razor

---
+++ Travels/Pages/Models/Places.cs
@@ -0,0 +1,13 @@
+namespace Travels.Models
+{
+    public class Places
+    {
+        public int ID {get; set; }
+        public string PlacesName {get; set; }
+        public string City {get; set; }
+        public string Thingstodo {get; set; }
+        public string name {get; set; }
+
+
+    }
+}
